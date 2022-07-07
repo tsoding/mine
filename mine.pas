bf15ed0542c9e568280eeca75c9c2c1626269f2c
rexim <reximkut@gmail.com>
2022-07-07 11:16:47 +0700

    Flood fill open

--- mine.pas
+++ mine.pas
@@ -56,7 +56,7 @@ type
          BombsCount := (Rows*Cols*BombsPercentage + 99) div 100;
          for Index := 1 to BombsCount do
          begin
-            { TODO: prevent this loop going indefinetly }
+            { TODO: prevent this loop going indefinitely }
             repeat
                RandomCell(Field, Row, Col)
             until (Cells[Row][Col] <> Bomb) and not IsAroundCursor(Field, Row, Col);
@@ -65,8 +65,42 @@ type
       end;
    end;
 
-   {TODO: flood fill open where it makes sense}
-   function OpenAtCursor(var Field: Field): Cell;
+   function FieldContains(Field: Field; Row, Col: Integer): Boolean;
+   begin
+      FieldContains := (0 <= Row) and (Row < Field.Rows) and (0 <= Col) and (Col < Field.Cols);
+   end;
+
+   function CountNborBombs(Field: Field; Row, Col: Integer): Integer;
+   var
+      DRow, DCol: Integer;
+   begin
+      CountNborBombs := 0;
+      with Field do
+         for DRow := -1 to 1 do
+            for DCol := -1 to 1 do
+               if (DRow <> 0) or (DCol <> 0) then
+                  if FieldContains(Field, Row + DRow, Col + DCol) then
+                     if Cells[Row + DRow][Col + DCol] = Bomb then
+                        Inc(CountNborBombs);
+   end;
+
+   function CountNborFlags(Field: Field; Row, Col: Integer): Integer;
+   var
+      DRow, DCol: Integer;
+   begin
+      CountNborFlags := 0;
+      with Field do
+         for DRow := -1 to 1 do
+            for DCol := -1 to 1 do
+               if (DRow <> 0) or (DCol <> 0) then
+                  if FieldContains(Field, Row + DRow, Col + DCol) then
+                     if States[Row + DRow][Col + DCol] = Flagged then
+                        Inc(CountNborFlags);
+   end;
+
+   function OpenAt(var Field: Field; Row, Col: Integer): Boolean;
+   var
+      DRow, DCol: Integer;
    begin
       with Field do
       begin
@@ -76,9 +110,23 @@ type
             Generated := True;
          end;
 
-         States[CursorRow][CursorCol] := Open;
-         OpenAtCursor := Cells[CursorRow][CursorCol];
+         States[Row][Col] := Open;
+
+         if CountNborBombs(Field, Row, Col) = CountNborFlags(Field, Row, Col) then
+            for DRow := -1 to 1 do
+               for DCol := -1 to 1 do
+                  if FieldContains(Field, DRow + Row, DCol + Col) then
+                     if States[DRow + Row][DCol + Col] = Closed then
+                        if OpenAt(Field, DRow + Row, DCol + Col) then
+                           Exit(True);
+
+         OpenAt := Cells[Row][Col] = Bomb;
       end
+   end;
+
+   function OpenAtCursor(var Field: Field): Boolean;
+   begin
+      OpenAtCursor := OpenAt(Field, Field.CursorRow, Field.CursorCol);
    end;
 
    {TODO: Open only unflagged bomb. Also indicate false flags somehow}
@@ -91,11 +139,6 @@ type
             for Col := 0 to Cols - 1 do
                if Cells[Row][Col] = Bomb then
                   States[Row][Col] := Open;
-   end;
-
-   function FieldContains(Field: Field; Row, Col: Integer): Boolean;
-   begin
-      FieldContains := (0 <= Row) and (Row < Field.Rows) and (0 <= Col) and (Col < Field.Cols);
    end;
 
    procedure FieldReset(var Field: Field; Rows, Cols: Integer);
@@ -119,20 +162,6 @@ type
       IsAtCursor := (Field.CursorRow = Row) and (Field.CursorCol = Col);
    end;
 
-   function CountNeighborBombs(Field: Field; Row, Col: Integer): Integer;
-   var
-      DRow, DCol: Integer;
-   begin
-      CountNeighborBombs := 0;
-      with Field do
-         for DRow := -1 to 1 do
-            for DCol := -1 to 1 do
-               if (DRow <> 0) or (DCol <> 0) then
-                  if FieldContains(Field, Row + DRow, Col + DCol) then
-                     if Cells[Row + DRow][Col + DCol] = Bomb then
-                        inc(CountNeighborBombs);
-   end;
-
    procedure FieldDisplay(Field: Field);
    var
       Row, Col, Nbors: Integer;
@@ -147,7 +176,7 @@ type
                   Open: case Cells[Row][Col] of
                            Bomb: Write('@');
                            Empty: begin
-                                     Nbors := CountNeighborBombs(Field, Row, Col);
+                                     Nbors := CountNborBombs(Field, Row, Col);
                                      if Nbors > 0 then Write(Nbors) else Write(' ');
                                   end;
                         end;
@@ -163,22 +192,22 @@ type
 
    procedure MoveUp(var Field: Field);
    begin
-      with Field do if CursorRow > 0 then dec(CursorRow);
+      with Field do if CursorRow > 0 then Dec(CursorRow);
    end;
 
    procedure MoveDown(var Field: Field);
    begin
-      with Field do if CursorRow < Rows-1 then inc(CursorRow);
+      with Field do if CursorRow < Rows-1 then Inc(CursorRow);
    end;
 
    procedure MoveLeft(var Field: Field);
    begin
-      with Field do if CursorCol > 0 then dec(CursorCol);
+      with Field do if CursorCol > 0 then Dec(CursorCol);
    end;
 
    procedure MoveRight(var Field: Field);
    begin
-      with Field do if CursorCol < Cols-1 then inc(CursorCol);
+      with Field do if CursorCol < Cols-1 then Inc(CursorCol);
    end;
 
 const
@@ -190,7 +219,10 @@ var
    Cmd: Char;
 begin
    Randomize;
-   {TODO: customizable size of the field}
+   {
+     TODO: customizable size of the field
+       Keep in mind that OpenAt is recursive. So at certain Field size we may get a Stack Overflow.
+   }
    FieldReset(MainField, 10, 10);
 
    if IsATTY(STDIN_FILENO) = 0 then
@@ -221,7 +253,7 @@ begin
          'q': break; {TODO: ask the user if they really want to exit. In case of accedental press of `q`}
          ' ': begin
                  {TODO: Victory condition (with a restart)}
-                 if OpenAtCursor(MainField) = Bomb then
+                 if OpenAtCursor(MainField) then
                  begin
                     OpenAllBombs(MainField);
                     Write(Chr(27), '[', MainField.Rows,   'A');
